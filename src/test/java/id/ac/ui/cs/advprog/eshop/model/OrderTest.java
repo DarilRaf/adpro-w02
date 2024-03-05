cbcef034db49c5feaa968ee5962f16106aaeee70
DarilRaf <darilmrafansyah@gmail.com>
2024-03-05 19:40:05 +0700

    clean some clutter

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
-import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
